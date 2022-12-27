0f9c6645b6f9a9a1ad99bf364614dd99bb600ecd
Kacper Slomski <kacperslomski1@gmail.com>
2022-12-27 18:15:02 +0100

    create ingredient model and implement listing them with tests

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -79,7 +79,7 @@ class PrivateTagsApiTests(TestCase):
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
 
-    def test_delete_ta(self):
+    def test_delete_tag(self):
         """Test deleting tag"""
         tag = Tag.objects.create(user=self.user, name='Breakfast')
 
