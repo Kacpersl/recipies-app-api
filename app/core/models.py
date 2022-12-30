d29c72548421534836b59abeec3bf90d16bf5c28
Kacper Slomski <kacperslomski1@gmail.com>
2022-12-30 19:30:14 +0100

    create image api

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,9 @@
 """
 DataBase models.
 """
+import uuid
+import os
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -9,6 +12,10 @@ from django.contrib.auth.models import (
     BaseUserManager,
 )
 
+def recipe_image_file_path(instance, filename):
+    """generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
 
 class UserManager(BaseUserManager):
     """manager for users"""
@@ -57,6 +64,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
